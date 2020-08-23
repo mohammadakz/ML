298d50f25a0522c94e12ca0cc512f5b0ea56d77b
Mohammad Akbarzadeh <akbarzadehm1996@gmail.com>
2020-08-23 16:29:57 -0400

    accuracy

--- Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import ListedColormap
 
